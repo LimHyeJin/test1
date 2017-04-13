bcf7f59a27d4ff560ca454bc399cc53ce06eef76
Lim <hjlim9898@gmail.com>
2017-04-13 10:52:17 +0900

    test_sub3 추가

--- Test1.java
+++ Test1.java
@@ -1,6 +1,6 @@
 ﻿public class Test1{
 	public static void main(String args[]){
-		System.out.println("세명컴퓨터고등학교");
+
 		System.out.println("1.고현서");
 		System.out.println("2.김경운");
 		System.out.println("3.김동현");
@@ -8,6 +8,7 @@
 		System.out.println("5.김민준");
 		System.out.println("6.김승찬");
 		System.out.println("7.김우영");
+		System.out.println("8.김준석");
 	}
 
 }
